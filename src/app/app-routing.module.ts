fc8a94accb578928f8a64fd83e3184be630d04b3
ZaherM7 <msaidie.zaher@yahoo.fr>
2021-10-29 14:21:43 +0200

    creation plante component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PlantesComponent } from './plantes/plantes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'plante', component: PlantesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
