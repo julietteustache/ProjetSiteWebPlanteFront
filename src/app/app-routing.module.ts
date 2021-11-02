577696e42332276e9165c61719af42cd39f4b909
julietteustache <juliette.eustache@icloud.com>
2021-11-02 10:11:35 +0100

    creation page annonce

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AnnonceComponent } from './annonce/annonce.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : 'annonce', component:AnnonceComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
