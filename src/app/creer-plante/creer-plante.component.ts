57b25cc7680f86716669e42d179262d2865aa5df
ZaherM7 <msaidie.zaher@yahoo.fr>
2021-11-02 10:44:40 +0100

    Création de la page et du formulaire pour ajouter des plantes

--- src/app/creer-plante/creer-plante.component.ts
+++ src/app/creer-plante/creer-plante.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-creer-plante',
@@ -6,10 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./creer-plante.component.css']
 })
 export class CreerPlanteComponent implements OnInit {
-
-  constructor() { }
+  msg:any;
+  plante:any;
+  constructor(private http:HttpClient,private route:Router) { }
 
   ngOnInit(): void {
+  }
+
+  creerPlante(val:any): void{
+    //console.log(val);
+    this.http.post('http://localhost:8085/creerplante', val).subscribe({
+      next:(data)=>{this.plante=data},
+      error:(err)=>{console.log(err)}
+    })
   }
 
 }
