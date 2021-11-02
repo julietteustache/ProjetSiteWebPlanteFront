ebad5684c2b3c924d75ece75f8a941a87daae287
Arthur Cornet <coarthur@esic.fr>
2021-11-02 16:24:21 +0100

    ajout du quizz pour calculer le score

---
+++ src/app/quizz-score/quizz-score.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { QuizzScoreComponent } from './quizz-score.component';
+
+describe('QuizzScoreComponent', () => {
+  let component: QuizzScoreComponent;
+  let fixture: ComponentFixture<QuizzScoreComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ QuizzScoreComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(QuizzScoreComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/quizz-score/quizz-score.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-quizz-score',
+  templateUrl: './quizz-score.component.html',
+  styleUrls: ['./quizz-score.component.css']
+})
+export class QuizzScoreComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
