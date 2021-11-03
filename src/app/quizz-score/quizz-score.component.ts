84ae5c0e2219bf83c64537c52b7e1874b56f1380
Arthur Cornet <coarthur@esic.fr>
2021-11-03 13:58:17 +0100

    modif score

--- src/app/quizz-score/quizz-score.component.ts
+++ src/app/quizz-score/quizz-score.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-quizz-score',
@@ -6,10 +8,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quizz-score.component.css']
 })
 export class QuizzScoreComponent implements OnInit {
+  msg: any;
+  user:any;
 
-  constructor() { }
+  constructor(private http:HttpClient,private route:Router) { }
 
   ngOnInit(): void {
+  }
+
+  calculscore(val:any): void{
+    this.http.post('http://localhost:8085/modifuser/{idUser}', val).subscribe({
+      next:(data)=>{this.user=data
+      this.route.navigateByUrl('quizz-score')},
+      error:(err)=>{console.log(err)}
+    })
   }
 
 }
