b67a0e7f5b95cd6ee098d23b50a7035feb8a61f6
ZaherM7 <msaidie.zaher@yahoo.fr>
2021-11-06 17:50:31 +0100

    dfn,elfkn

--- src/app/creer-plante/creer-plante.component.ts
+++ src/app/creer-plante/creer-plante.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-creer-plante',
@@ -8,21 +9,32 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./creer-plante.component.css']
 })
 export class CreerPlanteComponent implements OnInit {
-  msg:any;
-  plante:any;
-  constructor(private http:HttpClient,private route:Router) { }
+  msg: any;
+  plante: any;
+
+  user: any;
+  idUser: any;
+  score: any;
+  statut: any;
+
+  constructor(private http: HttpClient, private route: Router, private uConnect: AuthService) { }
 
   ngOnInit(): void {
   }
 
-  creerPlante(val:any): void{
-    //console.log(val);
-    this.http.post('http://localhost:8085/creerplante', val).subscribe({
-      next:(data)=>{this.plante=data;
-      this.route.navigateByUrl('liste_plantes');
-    },
-      error:(err)=>{console.log(err)}
-    })
+  creerPlante(val: any): void {
+    if (this.statut === "Petit cactus" || this.statut === "Primevère hivernale") {
+      this.msg = "Votre niveau n'est pas assez élevé";
+    }
+    else {
+      this.http.post('http://localhost:8085/creerplante', val).subscribe({
+        next: (data) => {
+          this.plante = data;
+          this.route.navigateByUrl('liste_plantes');
+        },
+        error: (err) => { console.log(err) }
+      })
+    }
   }
 
 }
