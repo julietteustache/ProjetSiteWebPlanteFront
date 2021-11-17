9d3ee10a9253a1505347cd96e1ad38b4067eb5e4
ZaherM7 <msaidie.zaher@yahoo.fr>
2021-11-17 17:24:03 +0100

    modif page publication

--- src/app/publications/publications.component.ts
+++ src/app/publications/publications.component.ts
@@ -1,4 +1,8 @@
+
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-publications',
@@ -6,10 +10,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./publications.component.css']
 })
 export class PublicationsComponent implements OnInit {
+  user:any;
+  idUser:any;
 
-  constructor() { }
+  publications:any;
+
+  constructor(private http:HttpClient, private route:Router, private uConnect:AuthService) { }
 
   ngOnInit(): void {
+    this.user=this.uConnect.getUserConnect();
+    this.idUser=this.user.idUser;
+    this.recupPublis();
+  }
+
+  recupPublis(): void {
+    this.http.get('http://localhost:8085/publications/' + this.idUser).subscribe({
+      next: (data) => {
+        this.publications = data;
+      },
+      error: (err) => { console.log(err) }
+
+    });
+  }
+
+  changeFormatMedia(media:any):any{
+    return window.atob(media);
+  }
+
+  mediaExist(media:any) : boolean {
+    if (media!=null) {
+      return true;
+    }
+    else {
+      return false;
+    }
   }
 
 }
